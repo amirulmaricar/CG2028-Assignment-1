8619bf6a318576d556648e1d4885a346032160f5
e0191729 <e0191729@u.nus.edu>
2018-09-12 16:15:16 +0800

    program will work for any M size lesser than 10

--- src/pdm.s
+++ src/pdm.s
@@ -7,14 +7,34 @@
 
 pdm:
 @ write the code for pdm(CM,M,index) here
-		STMDB SP!,{R3-R11} //Push all the data from R1-R12 to memory
 
-		SUBS R1, 1 //sIZE OF THE M -1
+		/** Here we push all data values from register 3-11
+		into the stack	*/
+
+		STMDB SP!,{R3-R11}
+
+		/** Here we subtract one from the order of
+		confusion matrix */
 		LDR R12, [R0]
-		MOV R3, R0
-		//LDR R11, [R0, #4]
-		//ADD R12, R12, R11
-		//ADD R0, R0, #4
+		MOV R3, R0 		//offset addresser
+
+		/** Here we calculate the offset mnumber for row
+		and lode the n(mm) value in R3 for devision */
+		MOV R4, #4
+		MUL R4, R4, R1
+		ADD R4, #4
+		MUL R4,R2,R4
+		ADD R3,R3,R4
+		LDR R3, [R3]
+		MOVW R4,#10000
+		MUL R3,R3,R4
+
+		SUBS R1, 1
+
+		/**
+
+		*/
+		@params effective address of R0
 LOOP:
 		LDR R11, [R0, #4]
 		ADD R12, R12, R11
@@ -22,17 +42,8 @@ LOOP:
         SUBS R1, 1
         BNE LOOP
 
-	MOV R4,#16 //MUST EDIT
-	MUL R4,R2,R4
-	ADD R3,R3,R4
-	LDR R3, [R3]
-	MOV R4,#1000 //EDIT
-	MUL R3,R3,R4
-	MOV R4,#10
-	MUL R3,R3,R4
+		UDIV R0,R3,R12
 
-	UDIV R0,R3,R12
+		LDMIA SP!,{R3-R11} // POP back the datas for R1-R12.
 
-	LDMIA SP!,{R3-R11} // POP back the datas for R1-R12.
-
-	BX	LR
+		BX	LR
